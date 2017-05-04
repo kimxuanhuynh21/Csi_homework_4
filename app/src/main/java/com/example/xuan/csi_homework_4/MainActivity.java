e64ac8687269a3b7f1408f7dd2ba1fe4b9aa4114
Xuan Huynh <kimxuanhuynh21@gmail.com>
2017-05-05 06:56:36 +0700

    Finish 2 buttons

--- app/src/main/java/com/example/xuan/csi_homework_4/MainActivity.java
+++ app/src/main/java/com/example/xuan/csi_homework_4/MainActivity.java
@@ -15,6 +15,7 @@ import com.github.lzyzsd.circleprogress.DonutProgress;
 import org.w3c.dom.Text;
 
 import java.util.Random;
+import java.util.StringTokenizer;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -32,14 +33,18 @@ public class MainActivity extends AppCompatActivity {
     Button startOverButton;
 
     TextView totalCookiesInJarTextView;
+    TextView timePassedTextView;
+    TextView firstMonsterEatenCookiesTextView;
+    TextView firstMonsterEatTimeLeftTextView;
+    TextView secondMonsterEatenCookiesTextView;
+    TextView secondMonsterEatTimeLeftTextView;
 
-    int firstMonsterEatenCookies = 0;
-    int secondMonsterEatenCookies = 0;
-    int totalCookiesInJar = 20;
-    int countDown = 0;
-    int grandmaMonsterCookingTime = 5000;
-
-    int gameDuration = 120;
+    int firstMonsterEatenCookies;
+    int secondMonsterEatenCookies;
+    int totalCookiesInJar;
+    int countDownClock;
+    int grandmaMonsterCookingTime;
+    int gameDuration;
 
     @Override
     protected void onCreate(Bundle icicle) {
@@ -47,8 +52,8 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_main);
 
+        initVariables();
         setControls();
-        startGame();
     }
 
     private void setControls(){
@@ -56,6 +61,11 @@ public class MainActivity extends AppCompatActivity {
         secondMonsterEatenProgressBar = (ProgressBar) findViewById(R.id.second_monster_eaten_progress_bar);
         countDownProgressBar = (ProgressBar) findViewById(R.id.count_down_progress_bar);
         totalCookiesInJarTextView = (TextView) findViewById(R.id.total_cookies_in_jar);
+        timePassedTextView = (TextView) findViewById(R.id.time_passed);
+        firstMonsterEatenCookiesTextView = (TextView) findViewById(R.id.first_monster_eaten_cookies);
+        firstMonsterEatTimeLeftTextView = (TextView) findViewById(R.id.first_monster_eat_time_left);
+        secondMonsterEatenCookiesTextView = (TextView) findViewById(R.id.second_monster_eaten_cookies);
+        secondMonsterEatTimeLeftTextView = (TextView) findViewById(R.id.second_monster_eat_time_left);
         cancelButton = (Button) findViewById(R.id.cancel_button);
         cancelButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -64,7 +74,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
         startOverButton = (Button) findViewById(R.id.start_over_button);
-        cancelButton.setOnClickListener(new View.OnClickListener() {
+        startOverButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 resumeGame();
@@ -76,6 +86,7 @@ public class MainActivity extends AppCompatActivity {
         handler.post(firstMonsterEating);
         handler.post(secondMonsterEating);
         handler.post(grandmaMonsterBaking);
+        handler.post(countDown);
     }
 
     private void pauseGame(){
@@ -85,6 +96,24 @@ public class MainActivity extends AppCompatActivity {
         handler.removeCallbacks(grandmaMonsterBaking);
     }
 
+    private void resetGame(){
+        initVariables();
+        startGame();
+    }
+
+    private void initVariables(){
+        firstMonsterEatenCookies = 0;
+        secondMonsterEatenCookies = 0;
+        totalCookiesInJar = 20;
+        countDownClock = 0;
+        grandmaMonsterCookingTime = 5000;
+        gameDuration = 120;
+    }
+
+    private boolean isGameStopped(){
+        return firstMonsterEatenCookies < 100 && secondMonsterEatenCookies < 100 && countDownClock < 120;
+    }
+
     private void resumeGame(){
         startGame();
     }
@@ -92,13 +121,18 @@ public class MainActivity extends AppCompatActivity {
     Runnable firstMonsterEating = new Runnable() {
         @Override
         public void run() {
-            int randomEatenCookies = random.nextInt(totalCookiesInJar);
-            int randomTime = random.nextInt(5000) + 5;
-            if(firstMonsterEatenCookies < 100){
+            if(isGameStopped()){
+                int randomEatenCookies = random.nextInt(totalCookiesInJar);
+                int randomTime = random.nextInt(4)+1;
                 firstMonsterEatenCookies += randomEatenCookies;
                 totalCookiesInJar -= randomEatenCookies;
+
+                firstMonsterEatenCookiesTextView.setText(String.valueOf(firstMonsterEatenCookies));
+                firstMonsterEatTimeLeftTextView.setText(String.valueOf(randomTime));
+                totalCookiesInJarTextView.setText(String.valueOf(totalCookiesInJar));
                 firstMonsterEatenProgressBar.setProgress(firstMonsterEatenCookies);
-                handler.postDelayed(this, randomTime);
+
+                handler.postDelayed(this, randomTime*1000);
                 Toast.makeText(MainActivity.this, "First monster consumes " + String.valueOf(randomEatenCookies) + " in " + String.valueOf(randomTime), Toast.LENGTH_SHORT).show();
             }
         }
@@ -106,13 +140,18 @@ public class MainActivity extends AppCompatActivity {
     Runnable secondMonsterEating = new Runnable() {
         @Override
         public void run() {
-            int randomEatenCookies = random.nextInt(totalCookiesInJar);
-            int randomTime = random.nextInt(5000) + 5;
-            if(secondMonsterEatenCookies < 100){
+            if(isGameStopped()){
+                int randomEatenCookies = random.nextInt(totalCookiesInJar);
+                int randomTime = random.nextInt(4)+1;
                 secondMonsterEatenCookies += randomEatenCookies;
                 totalCookiesInJar -= randomEatenCookies;
+
+                secondMonsterEatenCookiesTextView.setText(String.valueOf(secondMonsterEatenCookies));
+                secondMonsterEatTimeLeftTextView.setText(String.valueOf(randomTime));
+                totalCookiesInJarTextView.setText(String.valueOf(totalCookiesInJar));
                 secondMonsterEatenProgressBar.setProgress(secondMonsterEatenCookies);
-                handler.postDelayed(this, randomTime);
+
+                handler.postDelayed(this, randomTime*1000);
                 Toast.makeText(MainActivity.this, "Second monster consumes " + String.valueOf(randomEatenCookies) + " in " + String.valueOf(randomTime), Toast.LENGTH_SHORT).show();
             }
         }
@@ -122,9 +161,20 @@ public class MainActivity extends AppCompatActivity {
         public void run() {
             int randomCookies = random.nextInt(9) + 1;
             totalCookiesInJar += randomCookies;
-            totalCookiesInJarTextView.setText(String.valueOf(randomCookies));
+            totalCookiesInJarTextView.setText(String.valueOf(totalCookiesInJar));
             handler.postDelayed(this, grandmaMonsterCookingTime);
             Toast.makeText(MainActivity.this, "Grandma monster cooks " + String.valueOf(randomCookies), Toast.LENGTH_SHORT).show();
+        }
+    };
+    Runnable countDown = new Runnable() {
+        @Override
+        public void run() {
+            if(isGameStopped()){
+                countDownClock++;
+                timePassedTextView.setText(String.valueOf(countDownClock));
+                countDownProgressBar.setProgress(countDownClock);
+                handler.postDelayed(this, 1000);
+            }
         }
     };
 }
